b2b5c7cc73613e279ef525b50ae7f0cd54d03072
IT25102104 <it25102104@my.sliit.lk>
2025-07-13 00:58:10 +0530

    Add IT25102104Lab2Q2.java to Q2

---
+++ IT25102104Lab2Q2.java
@@ -0,0 +1,20 @@
+public class IT25102104Lab2Q2{
+	public static void main(String[]args) {
+		   //Perimeter of a Square = 4 * length
+		   //Circumference of Circle = 2 * PI * radius
+		   //PI =3.14
+		   
+		   double length = 10;
+		   double radious = 4*length/(2*3.14);
+		   
+		   //calculate the radious of the circular fence
+		    
+			  System.out.println("radious of the circular fence:" + radious);
+			  
+	}
+	
+	
+	
+}	
+
+		
